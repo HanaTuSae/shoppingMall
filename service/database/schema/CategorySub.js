144f477f9bcd32abd9f6cad251fb3b79d6c0a31d
HanaTuSae <zhengdingdong0307@qq.com>
2018-12-10 22:29:17 +0800

    定义小类数据模型

---
+++ service/database/schema/CategorySub.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose') // 引入Mongoose
+const Schema = mongoose.Schema // 声明Schema
+
+const categorySubSchema = new Schema({
+  ID: { unique: true, type: String },
+  MALL_CATEGORY_ID: { type: String },
+  MALL_SUB_NAME: { type: String },
+  COMMENTS: { type: String },
+  SORT: { type: Number }
+})
+
+mongoose.model('CategorySub', categorySubSchema)
